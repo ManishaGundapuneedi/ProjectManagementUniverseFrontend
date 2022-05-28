432c9b2aad58c9155308dfe31e594e3d81646acc
maneesha <maneesha@divami.com>
2022-05-28 09:01:24 +0530

    Add header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPageComponent from "../src/components/login-page/LoginPage";
 import ProjectsList from "./components/ProjectsList";
 import ResourcesList from "./components/ResourcesList";
+import Header from "./common/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route key="resources" path="/resources" component={ResourcesList} />
-        <Route key="projects" path="/projects" component={ProjectsList} />
-        <Route path="/" component={LoginPageComponent} />
-      </Routes>
+      <Router>
+        <Header />
+        <Routes>
+          <Route key="resources" path="/resources" element={<ResourcesList />} />
+          <Route key="projects" path="/projects" element={<ProjectsList />} />
+          <Route path="/" element={<LoginPageComponent />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/common/Header/Header.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import "./Header.scss";
+import HamburgerIcon from "../../assets/images/svgs/hamburger.svg";
+import CancelIcon from "../../assets/images/svgs/cancel.svg";
+import { Link } from "react-router-dom";
+
+const ITEMS_LIST = [
+  {
+    name: "Projects",
+    linkTo: "/projects",
+    className: "",
+  },
+  {
+    name: "Resources",
+    linkTo: "/resources",
+    className: "",
+  },
+  {
+    name: "Resource Utilisation",
+    linkTo: "/resource-utilisation",
+    className: "",
+  },
+  {
+    name: "Logout",
+    linkTo: "/",
+    className: "",
+  },
+];
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openHamburger: false,
+    };
+
+    this.handleHamburgerButtonClick =
+      this.handleHamburgerButtonClick.bind(this);
+  }
+
+  handleHamburgerButtonClick() {
+    this.setState((state) => {
+      return {
+        openHamburger: !state.openHamburger,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="headerContainer">
+        <div className="nav-brand">
+          <a class="navbar-brand-logo" href="###">
+            <img className="logoImg" src="" alt="brand-logo" />
+          </a>
+        </div>
+        <div className="nav-bar"></div>
+        <div className="hamburger">
+          <button
+            type="button"
+            className="addBtn"
+            onClick={this.handleHamburgerButtonClick}
+          >
+            <img
+              className="hamburgerImg"
+              src={HamburgerIcon}
+              alt="hamburger-icon"
+            />
+          </button>
+          {this.state.openHamburger && (
+            <div
+              className="hamburgerDropdown"
+              ref={this.hamburgerDropdown}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="hamburger-close-icon-section">
+                <img
+                  className="hamburger-cancelIcon"
+                  src={CancelIcon}
+                  alt="Cancel icon"
+                  onClick={this.handleHamburgerButtonClick}
+                />
+              </div>
+              <div className="hamburger-list-section">
+                <ul className="hamburger-list">
+                  {ITEMS_LIST.map((eachItem) => (
+                    <Link to={eachItem.linkTo}>
+                      <li className="hamburger-list-item">{eachItem.name}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
